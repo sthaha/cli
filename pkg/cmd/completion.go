7153c48ebece3c2209a38393e9ab0cc0fd252a8b
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2019-04-18 17:03:31 +1000

    completion: accept shell and positional arg

--- pkg/cmd/completion.go
+++ pkg/cmd/completion.go
@@ -21,11 +21,44 @@ import (
 )
 
 var completionCmd = &cobra.Command{
-	Use:   "completion",
-	Short: "Output bash completion code",
-	Run: func(cmd *cobra.Command, args []string) {
-		rootCmd.GenBashCompletion(os.Stdout)
+	Use:   "completion SHELL",
+	Short: "Prints shell completion scripts",
+	Long: `
+This command prints shell completion code which must be evaluated to provide interactive completion
+
+Supported Shells:
+  - bash
+`,
+	Args:      exactValidArgs(1),
+	ValidArgs: []string{"bash"},
+	Example: `
+  # generate completion code for bash
+  tkn completion bash > bash_completion.sh
+  source bash_completion.sh
+	`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		switch args[0] {
+		case "bash":
+			return rootCmd.GenBashCompletion(os.Stdout)
+			// TODO add zsh completion
+			// case "zsh":
+			// return rootCmd.GenZshCompletion(os.Stdout)
+			// NOTE: cobra v0.0.3 zsh completion seems fail on zsh 5.5.1
+		}
+		return nil
 	},
+}
+
+func exactValidArgs(n int) cobra.PositionalArgs {
+	nArgs := cobra.ExactArgs(n)
+
+	return func(cmd *cobra.Command, args []string) error {
+		if err := nArgs(cmd, args); err != nil {
+			return err
+		}
+		return cobra.OnlyValidArgs(cmd, args)
+	}
 }
 
 func init() {
